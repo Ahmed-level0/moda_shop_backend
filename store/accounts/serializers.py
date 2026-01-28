40690e16ef866e1a9f5f5aef3067ec981ebbf202
ahmed <ahmedhambuta9@gmail.com>
2026-01-28 02:53:10 +0200

    Edit: Solved the user deletion from admin panel problem and the social account problem and email confirmation

--- store/accounts/serializers.py
+++ store/accounts/serializers.py
@@ -3,21 +3,6 @@ from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 from rest_framework.authtoken.models import Token
 
-class SignupSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'password')
-
-    def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data.get('email'),
-            password=validated_data['password']
-        )
-        return user
-
 # Serializer for creating users
 class UserSerializer(ModelSerializer):
     class Meta:
--- store/store/settings.py
+++ store/store/settings.py
@@ -49,8 +49,18 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
 ]
 SITE_ID = 1
+
 LOGIN_REDIRECT_URL = '/'  
+
 LOGOUT_REDIRECT_URL = '/'
+
+ACCOUNT_AUTHENTICATION_METHOD = "email"
+ACCOUNT_EMAIL_REQUIRED = True
+USERNAME_REQUIRED = False
+
+ACCOUNT_EMAIL_VERIFICATION = "mandatory"
+ACCOUNT_CONFIRM_EMAIL_ON_GET = True
+LOGIN_ON_EMAIL_CONFIRMATION = True
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
@@ -88,10 +98,11 @@ ROOT_URLCONF = 'store.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -99,6 +110,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'store.wsgi.application'
 
@@ -153,7 +165,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
 
 MEDIA_URL = '/media/'
 
