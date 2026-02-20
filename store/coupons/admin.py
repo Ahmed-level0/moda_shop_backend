c7a2dfecfa9b4cc079505cb703ac67ca566c0356
ahmed <ahmedhambuta9@gmail.com>
2026-02-20 15:33:37 +0200

    MODIFY: Applied Coupons logic and implementation

--- store/coupons/admin.py
+++ store/coupons/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Coupon
 # Register your models here.
+
+@admin.register(Coupon)
+class CouponAdmin(admin.ModelAdmin):
+    list_display = ('id', 'code', 'discount', 'discount_type', 'valid_from', 'valid_until', 'usage_limit')
+    list_filter = ('discount_type',)
+    search_fields = ('code', 'id')
